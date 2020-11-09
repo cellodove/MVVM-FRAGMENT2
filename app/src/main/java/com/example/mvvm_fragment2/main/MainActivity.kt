52b93c7425c69b195518f7e4ea118df5ab7b5fd4
wntjd200@naver.com <wntjd200@naver.com>
2020-11-09 21:17:51 +0900

    init

---
+++ app/src/main/java/com/example/mvvm_fragment2/main/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.mvvm_fragment2.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mvvm_fragment2.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        
+    }
+}
